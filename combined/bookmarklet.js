0691ea525f6e356354962b3dce79d44636576349
kybernetikos <kybernetikos@gmail.com>
2012-08-07 19:34:04 +0100

    Fixed bookmarklet

--- combined/bookmarklet.js
+++ combined/bookmarklet.js
@@ -1,12 +1,13 @@
 (function() {
+	var global = this;
 	var css = document.createElement('link');
 	css.setAttribute('rel', 'stylesheet');
 	css.setAttribute('href', 'http://kybernetikos.github.com/SoleMirror/combined/combined.css');
 	document.head.appendChild(css);
 	var js = document.createElement('script');
 	js.setAttribute('src', 'http://kybernetikos.github.com/SoleMirror/combined/combined.js');
+	js.onload = function() {
+		global.con = new SoleMirror();
+	};
 	document.head.appendChild(js);
-	var open = document.createElement('script');
-	open.innerText = "var con = new SoleMirror();";
-	document.head.appendChild(open);
 })();
