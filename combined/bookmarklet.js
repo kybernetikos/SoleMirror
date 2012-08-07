538d339fd24ff0f9bbe671de99ddac44e1a106f1
kybernetikos <kybernetikos@gmail.com>
2012-08-07 22:06:44 +0100

    Bookmarklet improvements

--- combined/bookmarklet.js
+++ combined/bookmarklet.js
@@ -1,13 +1,23 @@
 (function() {
 	var global = this;
-	var css = document.createElement('link');
-	css.setAttribute('rel', 'stylesheet');
-	css.setAttribute('href', 'http://kybernetikos.github.com/SoleMirror/combined/combined.css');
-	document.head.appendChild(css);
-	var js = document.createElement('script');
-	js.setAttribute('src', 'http://kybernetikos.github.com/SoleMirror/combined/combined.js');
-	js.onload = function() {
-		global.con = new SoleMirror();
-	};
-	document.head.appendChild(js);
+	if (global.SoleMirror) {
+		global.soleMirrorDiv.style.display = global.soleMirrorVisible ? 'none' : 'block';
+		global.soleMirrorVisible = ! global.soleMirrorVisible;
+	} else {
+		var css = document.createElement('link');
+		css.setAttribute('rel', 'stylesheet');
+		css.setAttribute('href', 'http://kybernetikos.github.com/SoleMirror/combined/combined.css');
+		document.head.appendChild(css);
+		var js = document.createElement('script');
+		js.setAttribute('src', 'http://kybernetikos.github.com/SoleMirror/combined/combined.js');
+		js.onload = function() {
+			var console = document.createElement('div');
+			console.setAttribute('id', 'SoleMirror');
+			document.body.appendChild(console);
+			global.con = new SoleMirror(console);
+			global.soleMirrorDiv = console;
+			global.soleMirrorVisible = true;
+		};
+		document.head.appendChild(js);
+	}
 })();
